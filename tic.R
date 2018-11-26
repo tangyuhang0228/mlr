8ce6ae6b0abd021737826fc28b2675639d21b624
pat-s <patrick.schratz@gmail.com>
2018-11-26 10:29:07 +0100

    various changes

--- tic.R
+++ tic.R
@@ -26,7 +26,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
   get_stage("script") %>%
     add_code_step(pkgbuild::compile_dll()) %>%
     add_code_step(devtools::document()) %>%
-    add_step(step_rcmdcheck("--as-cran", warnings_are_errors = FALSE, notes_are_errors = FALSE))
+    add_step(step_rcmdcheck("--as-cran", error_on = "error"))
 
   # only deploy in master branch
   if (ci()$get_branch() == "master") {
@@ -37,7 +37,8 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
     get_stage("deploy") %>%
       add_code_step(pkgbuild::compile_dll()) %>%
       add_code_step(devtools::document()) %>%
-      add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
+      add_code_step(system("bash addon/convert_to_ascii_news.sh")) %>%
+      add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE", "NEWS")))
   }
 }
 
@@ -45,12 +46,15 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
 
   get_stage("install") %>%
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0)
-      install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
-    add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
+      install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]))
+
+  if (inherits(ci(), "TravisCI")) {
+    get_stage("before_script") %>%
+      add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
+                                             "-install-package", "thirdparty/XMeans1.0.4.zip")))
+  }
 
   get_stage("install") %>%
-    add_step(step_install_cran("magick")) %>% # favicon creation
     add_step(step_install_cran("pander"))
 
     get_stage("before_deploy") %>%
