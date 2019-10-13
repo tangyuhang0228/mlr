a182492a5c8baf863572cc8b394f483f105f5320
pat-s <patrick.schratz@gmail.com>
2019-10-13 10:23:41 +0200

    try

---
+++ tests/run-all.R
@@ -0,0 +1,13 @@
+library("future.callr")
+library("here")
+library("doFuture")
+registerDoFuture()
+
+plan(callr, workers = 4)
+
+files = list.files(here("tests/tests-seq"), full.names = TRUE)
+files = files[-16]
+
+foreach (i = files) %dopar% {
+  source(i)
+}
--- tic.R
+++ tic.R
@@ -1,6 +1,9 @@
 get_stage("script") %>%
   add_code_step(RWeka::WPM("refresh-cache")) %>%
-  add_code_step(RWeka::WPM('install-package', 'XMeans'))
+  add_code_step(RWeka::WPM('install-package', 'XMeans')) %>%
+  add_step(step_install_cran("doFuture")) %>%
+  add_step(step_install_cran("future.callr")) %>%
+  add_step(step_install_cran("here"))
 
 # R CMD Check
 do_package_checks(args = "--as-cran", error_on = "error",
